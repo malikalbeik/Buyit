d3999e55078d15acff2e04601db227919156dcc8
Malik Albeik <abd.beik@me.com>
2018-07-12 13:09:55 +0300

    added functions for database management

--- app.py
+++ app.py
@@ -26,3 +26,44 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# Connecting the sqlite3 database
+DATABASE = 'buyit.db'
+
+
+def get_db():
+    """Connects the database"""
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+def insert(table, fields=(), values=()):
+    """Inserts values into database"""
+    cur = get_db()
+    query = 'INSERT INTO %s (%s) VALUES (%s)' % (
+        table,
+        ', '.join(fields),
+        ', '.join(['?'] * len(values))
+    )
+    cur.execute(query, values)
+    cur.commit()
+    # id = cur.lastrowid
+    cur.close()
+    return True
+    # return id
+
+@app.teardown_appcontext
+def close_connection(exception):
+    """Disconnects the database"""
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    """Selects rows from the database"""
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
