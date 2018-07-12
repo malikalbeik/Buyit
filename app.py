fe9d7696a6fbeeb244adf0e7252274ac52e6452b
Malik Albeik <abd.beik@me.com>
2018-07-12 13:07:31 +0300

    initial app.py setup

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, flash, redirect, render_template, request, session, url_for, g
+from tempfile import mkdtemp
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+import sqlite3
+
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
