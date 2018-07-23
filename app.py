75972196e72d093417f57907fb5cad2c3ef1c915
Malik Albeik <abd.beik@me.com>
2018-07-23 20:12:04 +0300

    Added the login required function to prohibit the users who are not loged in to access some pages

--- app.py
+++ app.py
@@ -5,6 +5,19 @@ from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
 
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
 
 # Configure application
 app = Flask(__name__)
